a23f8b0928d1c82857892058698f3b42ece35f63
Alan Hamlett <alan.hamlett@gmail.com>
2026-04-15 09:52:52 -0400

    nvim init config

--- install.sh
+++ install.sh
@@ -2,20 +2,14 @@
 
 set -euo pipefail
 
-brew install vim
+brew install neovim ripgrep
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone git://github.com/alanhamlett/dotfiles.git
-ln -sf $PWD/dotfiles/vimrc $HOME/.vimrc
-rm -f $HOME/.zshrc || true
-ln -sf $PWD/dotfiles/zshrc $HOME/.zshrc
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-brew tap universal-ctags/universal-ctags
-brew install --HEAD universal-ctags
-mkdir ~/.npm-global
-npm config set prefix '~/.npm-global'
-npm -g install instant-markdown-d import-js
-sudo pip install flake8
-ln -sf `which flake8` $HOME/.config/flake8
-vim +PluginInstall +qall
-brew install yarn
-cd $HOME/.vim/bundle/vim-prettier; yarn install; cd $OLDPWD
+mkdir -p "$HOME/.config/nvim"
+ln -sf "$PWD/dotfiles/nvim_init.lua" "$HOME/.config/nvim/init.lua"
+ln -sf "$PWD/dotfiles/vimrc" "$HOME/.vimrc"
+rm -f "$HOME/.zshrc" || true
+ln -sf "$PWD/dotfiles/zshrc" "$HOME/.zshrc"
+
+# First launch installs lazy.nvim and all plugins automatically
+nvim --headless "+Lazy! sync" +qa
---
+++ nvim_init.lua
@@ -0,0 +1,483 @@
+--
+-- NeoVim Configuration
+--   Author: Alan Hamlett
+--   Migrated from Vim/Vundle to NeoVim/lazy.nvim
+--
+
+-- Suppress vim.tbl_flatten deprecation warnings from plugins
+vim.tbl_flatten = function(t)
+  return vim.iter(t):flatten():totable()
+end
+
+-- ============================================================================
+-- General Settings
+-- ============================================================================
+
+vim.g.mapleader = "\\"
+vim.g.maplocalleader = "\\"
+
+vim.opt.encoding = "utf-8"
+vim.opt.showcmd = true
+vim.opt.hidden = true
+vim.opt.errorbells = false
+vim.opt.backspace = "indent,eol,start"
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 2
+vim.opt.softtabstop = 2
+vim.opt.tabstop = 4
+vim.opt.autoindent = true
+vim.opt.number = true
+vim.opt.showmatch = true
+vim.opt.incsearch = true
+vim.opt.hlsearch = true
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.wildmenu = true
+vim.opt.wildmode = "list:longest,full"
+vim.opt.scrolloff = 2
+vim.opt.laststatus = 2
+vim.opt.ruler = true
+vim.opt.wrap = false
+vim.opt.synmaxcol = 900
+vim.opt.fileformats = "unix,mac,dos"
+vim.opt.colorcolumn = "80"
+vim.opt.termguicolors = true
+vim.opt.signcolumn = "yes"
+vim.opt.updatetime = 300
+
+-- Folding
+vim.opt.foldenable = true
+vim.opt.foldmethod = "indent"
+vim.opt.foldlevel = 0
+vim.opt.foldopen = "block,hor,mark,percent,quickfix,tag"
+
+-- Backup, swap, views
+vim.fn.mkdir(vim.fn.expand("~/.vim/.backups"), "p")
+vim.fn.mkdir(vim.fn.expand("~/.vim/.swaps"), "p")
+vim.fn.mkdir(vim.fn.expand("~/.vim/.views"), "p")
+vim.opt.backup = true
+vim.opt.backupdir = vim.fn.expand("~/.vim/.backups/")
+vim.opt.directory = vim.fn.expand("~/.vim/.swaps/")
+vim.opt.viewdir = vim.fn.expand("~/.vim/.views/")
+
+-- ============================================================================
+-- Filetype-specific settings
+-- ============================================================================
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "javascript",
+  callback = function()
+    vim.opt_local.shiftwidth = 2
+    vim.opt_local.softtabstop = 2
+    vim.opt_local.foldmethod = "marker"
+    vim.opt_local.cindent = false
+    vim.opt_local.smartindent = true
+    vim.opt_local.colorcolumn = "160"
+    vim.opt_local.iskeyword:append("$")
+  end,
+})
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "typescript",
+  callback = function()
+    vim.opt_local.shiftwidth = 2
+    vim.opt_local.softtabstop = 2
+    vim.opt_local.colorcolumn = "160"
+  end,
+})
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "typescriptreact",
+  callback = function()
+    vim.opt_local.shiftwidth = 2
+    vim.opt_local.softtabstop = 2
+    vim.opt_local.colorcolumn = "160"
+  end,
+})
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "python",
+  callback = function()
+    vim.opt_local.shiftwidth = 4
+    vim.opt_local.softtabstop = 4
+    vim.opt_local.colorcolumn = "160"
+  end,
+})
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "go",
+  callback = function()
+    vim.opt_local.shiftwidth = 4
+    vim.opt_local.softtabstop = 4
+    vim.opt_local.expandtab = false
+  end,
+})
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "html", "htmldjango", "css", "less", "scss" },
+  callback = function()
+    vim.opt_local.shiftwidth = 2
+    vim.opt_local.softtabstop = 2
+    vim.opt_local.cindent = false
+    vim.opt_local.smartindent = true
+  end,
+})
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "ruby",
+  callback = function()
+    vim.opt_local.shiftwidth = 2
+    vim.opt_local.softtabstop = 2
+    vim.opt_local.colorcolumn = "100"
+  end,
+})
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "perl",
+  callback = function()
+    vim.opt_local.shiftwidth = 4
+    vim.opt_local.softtabstop = 4
+    vim.opt_local.foldmethod = "marker"
+    vim.opt_local.iskeyword:append("@-@,$,%")
+  end,
+})
+
+-- Strip trailing whitespace on save
+vim.api.nvim_create_autocmd("BufWritePre", {
+  pattern = "*",
+  callback = function()
+    local pos = vim.api.nvim_win_get_cursor(0)
+    vim.cmd([[silent! %s/\s\+$//e]])
+    vim.api.nvim_win_set_cursor(0, pos)
+  end,
+})
+
+-- Restore cursor position when reopening files
+vim.api.nvim_create_autocmd("BufReadPost", {
+  pattern = "*",
+  callback = function()
+    local mark = vim.api.nvim_buf_get_mark(0, '"')
+    if mark[1] > 0 and mark[1] <= vim.api.nvim_buf_line_count(0) then
+      vim.api.nvim_win_set_cursor(0, mark)
+    end
+  end,
+})
+
+-- Highlight tabs as errors in non-Go files
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "*",
+  callback = function()
+    if vim.bo.filetype ~= "go" then
+      vim.fn.matchadd("Error", "\\s*\\t\\+")
+    end
+  end,
+})
+
+-- ============================================================================
+-- Key Mappings
+-- ============================================================================
+
+local map = vim.keymap.set
+
+-- Pressing v escapes from visual mode
+map("v", "v", "<Esc>", { desc = "Exit visual mode" })
+
+-- Space centers current line
+map("n", "<Space>", "zvzz", { desc = "Open fold and center" })
+
+-- Up/Down scroll one line
+map("n", "<Up>", "<C-y>", { desc = "Scroll up" })
+map("n", "<Down>", "<C-e>", { desc = "Scroll down" })
+
+-- Left/Right move to prev/next fold
+map("n", "<Left>", "zk", { desc = "Previous fold" })
+map("n", "<Right>", "zj", { desc = "Next fold" })
+
+-- Fold level shortcuts
+for i = 0, 9 do
+  map("n", "f" .. i, ":set foldlevel=" .. i .. "<CR>", { desc = "Set fold level " .. i })
+end
+
+-- Shift+Backspace = Backspace
+map({ "n", "i" }, "<S-BS>", "<BS>")
+map({ "n", "i" }, "<Del>", "<BS>")
+map({ "n", "i" }, "<S-Del>", "<BS>")
+
+-- Window navigation with Ctrl+h/j/k/l
+map("n", "<C-h>", "<C-w>h", { desc = "Window left" })
+map("n", "<C-j>", "<C-w>j", { desc = "Window down" })
+map("n", "<C-k>", "<C-w>k", { desc = "Window up" })
+map("n", "<C-l>", "<C-w>l", { desc = "Window right" })
+
+-- Fix syntax highlighting
+map("n", "<C-s>", ":syntax sync fromstart<CR>", { desc = "Resync syntax" })
+
+-- ============================================================================
+-- Plugin Manager (lazy.nvim)
+-- ============================================================================
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable",
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- ============================================================================
+-- Plugins
+-- ============================================================================
+
+require("lazy").setup({
+
+  -- Solarized color scheme
+  {
+    "maxmx03/solarized.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require("solarized").setup({
+        transparent = { enabled = true },
+      })
+      vim.o.background = "dark"
+      vim.cmd.colorscheme("solarized")
+    end,
+  },
+
+  -- CamelCase motion (use , as motion leader: ,w ,b ,e ,ge)
+  {
+    "bkad/CamelCaseMotion",
+    init = function()
+      vim.g.camelcasemotion_key = ","
+    end,
+  },
+
+  -- Buffer tabs at top
+  {
+    "akinsho/bufferline.nvim",
+    version = "*",
+    dependencies = "nvim-tree/nvim-web-devicons",
+    config = function()
+      require("bufferline").setup({
+        options = {
+          mode = "buffers",
+          diagnostics = "nvim_lsp",
+          show_buffer_close_icons = false,
+          show_close_icon = false,
+          separator_style = "thin",
+        },
+      })
+      -- Buffer navigation (matching your Ctrl-n/p/d mappings)
+      map("n", "<C-n>", ":BufferLineCycleNext<CR>", { silent = true, desc = "Next buffer" })
+      map("n", "<C-p>", ":BufferLineCyclePrev<CR>", { silent = true, desc = "Previous buffer" })
+      map("n", "<C-d>", ":bdelete<CR>", { silent = true, desc = "Delete buffer" })
+    end,
+  },
+
+  -- Statusline (replaces vim-airline)
+  {
+    "nvim-lualine/lualine.nvim",
+    dependencies = "nvim-tree/nvim-web-devicons",
+    config = function()
+      require("lualine").setup({
+        options = {
+          theme = "solarized_dark",
+        },
+      })
+    end,
+  },
+
+  -- File explorer (replaces NERDTree)
+  {
+    "nvim-tree/nvim-tree.lua",
+    dependencies = "nvim-tree/nvim-web-devicons",
+    config = function()
+      require("nvim-tree").setup({
+        view = { width = 30 },
+      })
+      map("n", "<C-t>", ":NvimTreeToggle<CR>", { silent = true, desc = "Toggle file tree" })
+    end,
+  },
+
+  -- Fuzzy finder (replaces ctrlp)
+  {
+    "nvim-telescope/telescope.nvim",
+    branch = "0.1.x",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+      local builtin = require("telescope.builtin")
+      map("n", "<C-f>", builtin.find_files, { desc = "Find files" })
+      map("n", "<leader>fg", builtin.live_grep, { desc = "Live grep" })
+      map("n", "<leader>fb", builtin.buffers, { desc = "Find buffers" })
+    end,
+  },
+
+  -- Treesitter for better syntax highlighting
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function()
+      require("nvim-treesitter.configs").setup({
+        ensure_installed = {
+          "typescript", "tsx", "javascript",
+          "go", "gomod", "gosum",
+          "python",
+          "lua", "vim", "vimdoc",
+          "html", "css", "json", "yaml", "markdown",
+        },
+        highlight = { enable = true },
+        indent = { enable = true },
+      })
+    end,
+  },
+
+  -- LSP
+  {
+    "neovim/nvim-lspconfig",
+    dependencies = {
+      "mason-org/mason.nvim",
+      "mason-org/mason-lspconfig.nvim",
+    },
+    config = function()
+      require("mason").setup()
+      require("mason-lspconfig").setup({
+        ensure_installed = {
+          "tsserver",      -- TypeScript
+          "gopls",         -- Go
+          "pyright",       -- Python
+        },
+      })
+
+      local lspconfig = require("lspconfig")
+      local on_attach = function(_, bufnr)
+        local opts = { buffer = bufnr, silent = true }
+        map("n", "<C-g>", vim.lsp.buf.definition, opts)
+        map("n", "gd", vim.lsp.buf.definition, opts)
+        map("n", "K", vim.lsp.buf.hover, opts)
+        map("n", "gr", vim.lsp.buf.references, opts)
+        map("n", "<leader>rn", vim.lsp.buf.rename, opts)
+        map("n", "<leader>ca", vim.lsp.buf.code_action, opts)
+        map("n", "[d", vim.diagnostic.goto_prev, opts)
+        map("n", "]d", vim.diagnostic.goto_next, opts)
+      end
+
+      local servers = { "tsserver", "gopls", "pyright" }
+      for _, server in ipairs(servers) do
+        lspconfig[server].setup({ on_attach = on_attach })
+      end
+    end,
+  },
+
+  -- Autocompletion
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-buffer",
+      "hrsh7th/cmp-path",
+      "L3MON4D3/LuaSnip",
+      "saadparwaiz1/cmp_luasnip",
+    },
+    config = function()
+      local cmp = require("cmp")
+      local luasnip = require("luasnip")
+      cmp.setup({
+        snippet = {
+          expand = function(args)
+            luasnip.lsp_expand(args.body)
+          end,
+        },
+        mapping = cmp.mapping.preset.insert({
+          ["<Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_next_item()
+            elseif luasnip.expand_or_jumpable() then
+              luasnip.expand_or_jump()
+            else
+              fallback()
+            end
+          end, { "i", "s" }),
+          ["<S-Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_prev_item()
+            elseif luasnip.jumpable(-1) then
+              luasnip.jump(-1)
+            else
+              fallback()
+            end
+          end, { "i", "s" }),
+          ["<CR>"] = cmp.mapping.confirm({ select = true }),
+          ["<C-Space>"] = cmp.mapping.complete(),
+        }),
+        sources = cmp.config.sources({
+          { name = "nvim_lsp" },
+          { name = "luasnip" },
+        }, {
+          { name = "buffer" },
+          { name = "path" },
+        }),
+      })
+    end,
+  },
+
+  -- Linting (async, runs while you type)
+  {
+    "mfussenegger/nvim-lint",
+    config = function()
+      local lint = require("lint")
+      lint.linters_by_ft = {
+        python = { "ruff" },
+        typescript = { "eslint" },
+        typescriptreact = { "eslint" },
+        javascript = { "eslint" },
+        javascriptreact = { "eslint" },
+        go = { "golangcilint" },
+      }
+      -- Lint on save AND while typing (debounced via TextChanged)
+      vim.api.nvim_create_autocmd({ "BufWritePost", "TextChanged", "InsertLeave", "BufEnter" }, {
+        callback = function()
+          lint.try_lint()
+        end,
+      })
+    end,
+  },
+
+  -- Git signs in gutter
+  {
+    "lewis6991/gitsigns.nvim",
+    config = function()
+      require("gitsigns").setup()
+    end,
+  },
+
+  -- Surround (replaces vim-surround)
+  {
+    "kylechui/nvim-surround",
+    version = "*",
+    event = "VeryLazy",
+    config = function()
+      require("nvim-surround").setup()
+    end,
+  },
+
+  -- WakaTime
+  { "wakatime/vim-wakatime", lazy = false },
+
+  -- Go support
+  {
+    "fatih/vim-go",
+    ft = "go",
+    build = ":GoUpdateBinaries",
+  },
+
+}, {
+  -- lazy.nvim UI config
+  ui = {
+    border = "rounded",
+  },
+})
