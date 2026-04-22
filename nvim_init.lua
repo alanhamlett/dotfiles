3f7ae838fcbf93c7156389b6a5b603bfd7c61c63
Alan Hamlett <alan.hamlett@gmail.com>
2026-04-22 10:56:10 -0400

    use buffer id instead of ordinal

--- nvim_init.lua
+++ nvim_init.lua
@@ -307,7 +307,7 @@ require("lazy").setup({
       require("bufferline").setup({
         options = {
           mode = "buffers",
-          numbers = "ordinal",
+          numbers = "buffer_id",
           diagnostics = "nvim_lsp",
           show_buffer_icons = false,
           show_buffer_close_icons = false,
